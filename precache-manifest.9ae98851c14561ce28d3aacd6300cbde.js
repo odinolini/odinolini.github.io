2860cadebfdb11e98a24c5d6b0161a66c512fa9d
odinolini <aoandberg@gmail.com>
2018-11-08 21:23:48 +0100

    Updates

---
+++ precache-manifest.9ae98851c14561ce28d3aacd6300cbde.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1de806140aded5c553bb",
+    "url": "/keyboard-test/static/js/runtime~main.1de80614.js"
+  },
+  {
+    "revision": "56899639b6300e18763b",
+    "url": "/keyboard-test/static/js/main.56899639.chunk.js"
+  },
+  {
+    "revision": "cd7e9c3a987592720411",
+    "url": "/keyboard-test/static/js/1.cd7e9c3a.chunk.js"
+  },
+  {
+    "revision": "56899639b6300e18763b",
+    "url": "/keyboard-test/static/css/main.c7802d6d.chunk.css"
+  },
+  {
+    "revision": "7e7478ed9ac1f1b0a6edcd889b221e84",
+    "url": "/keyboard-test/index.html"
+  }
+];
